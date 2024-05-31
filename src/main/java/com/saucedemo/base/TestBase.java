0bca092ff7e2321a54ed1f42a531bb15dc8abf5d
Gulnaz Safiullina <safiullinagulnazild@gmail.com>
2024-05-31 20:00:53 +0300

    WebEvent Listener, Constants added

--- src/main/java/com/saucedemo/base/TestBase.java
+++ src/main/java/com/saucedemo/base/TestBase.java
@@ -1,12 +1,17 @@
 package com.saucedemo.base;
 
+import com.saucedemo.constants.TestConstants;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.time.Duration;
 import java.util.Arrays;
@@ -19,12 +24,49 @@ public class TestBase {
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    public 
+    private void loadProperties() {
+        properties = new Properties();
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Failed to load properties file");
+        }
+    }
 
-    public void setUp () {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://www.saucedemo.com/");
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public int getIntProperty(String key) {
+        String value = properties.getProperty(key);
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Failed to convert property value to int: " + value);
+        }
+    }
+
+    public boolean getBooleanProperty(String key) {
+        return Boolean.parseBoolean(properties.getProperty(key));
+    }
+
+    public void setUp () throws Exception {
+        String browser = getProperty("driver");
+        if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+        }
+        else if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else {
+            throw new Exception("Incorrect Browser");
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestConstants.IMPLICIT_WAIT));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestConstants.PAGELOAD_TIMEOUT));
+        driver.get(getProperty("url"));
     }
 
     public void tearDown() {
---
+++ src/main/java/com/saucedemo/constants/TestConstants.java
@@ -0,0 +1,10 @@
+package com.saucedemo.constants;
+
+public class TestConstants {
+
+    public static final int PAGELOAD_TIMEOUT = 20;
+
+    public static final int IMPLICIT_WAIT =10;
+
+
+}
